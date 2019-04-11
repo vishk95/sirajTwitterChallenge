10a3a9d8607a4c053418c65c2ea1a37d2cfffd9c
vishk95 <vishalat1994@gmail.com>
2019-04-11 23:07:26 +0530

    Update Siraj2assignment.py

--- Siraj2assignment.py
+++ Siraj2assignment.py
@@ -1,18 +1,18 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'vLAzTkrQD6OiY3o2ZLE7nkzrA'
-consumer_secret = 'UWkNBN7m0S95MShyUX36ekeDasUGGwZSzoHOVrP94PcBK3IJBu'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '448924543-N32fjI9BMaSyJ9xUARk02dAnWaE5wjuGgbWhkzXK'
-access_token_secret = 'GlGnQAwWh5WIpYO3KBkPsPwdRk1uDC240zJoQ6UUf9IJ9'
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search('Black hole')
+public_tweets = api.search('Mars')
 
 import csv
 with open('twitter.csv', 'w') as csvfile:
